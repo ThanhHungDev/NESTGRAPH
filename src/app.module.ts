5c3e6ae709f210b39d99df9d366bd47920a7bca0
hungttahippo <htruong@ahippo.com>
2022-11-16 08:50:44 +0700

    update apolo v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: true
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     UsersModule,
   ],
